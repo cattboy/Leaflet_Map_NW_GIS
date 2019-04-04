92e4bbd31035bfd09c49e89cc8af6b1960aac2e5
cattboy <40659219+cattboy@users.noreply.github.com>
2019-04-04 19:28:28 +1000

    Delete success.php

--- login/includes/success.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    //Only access this page if session variable is true
-    if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in']) == false) {
-	
-    header("Location: http://nwmap.pcriot.com/BlackPad_NewMap.html");
-    }
-
- ?>
