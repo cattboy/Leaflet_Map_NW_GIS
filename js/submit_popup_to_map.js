0eec84647e081a379cabdf99bf5ee13e6efceb2d
cattboy <40659219+cattboy@users.noreply.github.com>
2019-04-06 11:01:19 +1000

    Update submit_popup_to_map.js

--- js/submit_popup_to_map.js
+++ js/submit_popup_to_map.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 	document.querySelector("#node_submission_form2").addEventListener("submit", function(e){
 	    if(!doValidation()){
 	        e.preventDefault();    //stop form from submitting
-	        alert(selectedValue);
+	        //alert(selectedValue);
 	        //add database submissions here
 	        
 
@@ -12,72 +12,80 @@ $(document).ready(function()
 	    }
 	});
 
+ 	
+
+
+
 	function doValidation() //Make sure they selected a Harvesting type, and return the one theey selected
 	{
-	 var ddl = document.getElementById("node_resource");
-	 var selectedValue = ddl.options[ddl.selectedIndex].value;
-	    if (selectedValue == "disabled")
-	    {
-	    	alert("Please Select Harvesting Type!");
-	    	exit();
-	    }
-	    else if(selectedValue == "mining_pick") // maybe change to arr.indexOf(selectedValue) (will check my array called ARR for a
-	    {
+		//Need to make global
+		var myStringArray_node_resource = 
+		[
+		"resource_mining",
+		"resource_lumberjacking",
+		"resource_sickle",
+		"resource_monster",
+		"resource_hands",
+		"resource_poi"
+		];
 
-	    	alert(selectedValue);
-	        var dd2 = document.getElementById("mining_pick");
-	        var selectedValue2 = dd2.options[dd2.selectedIndex].value
-	        alert(selectedValue2);
+		var myStringArray_resource_mining = 
+		[
+		"resource_iron_ore",
+		"resource_gold_ore",
+		"resource_silver_ore"
+		];
+		var myStringArray_resource_lumberjacking = 
+		[
+		"resource_wyrdwood",
+		"resource_ironwood"
+		];
+		//Need one for sickle
+		//One for poi, for each type
+		//^^^^^^^^^^^
 
-	    }
-	   	else if(selectedValue == "lumberjacking_axe")
-	    {
-
-	    	alert(selectedValue);
-	        var dd2 = document.getElementById("lumberjacking_axe");
-	        var selectedValue2 = dd2.options[dd2.selectedIndex].value
-	        alert(selectedValue2);
-
-	    }
-	    else if(selectedValue == "sickle_")
-	    {
-
-	    	alert(selectedValue);
-	        var dd2 = document.getElementById("sickle_");
-	        var selectedValue2 = dd2.options[dd2.selectedIndex].value
-	        alert(selectedValue2);
-
-	    }
-	    else if(selectedValue == "skinning_knife")
-	    {
-
-	    	alert(selectedValue);
-	        var dd2 = document.getElementById("skinning_knife");
-	        var selectedValue2 = dd2.options[dd2.selectedIndex].value
-	        alert(selectedValue2);
-
-	    }
-	    else if(selectedValue == "hands_")
-	    {
-
-	    	alert(selectedValue);
-	        var dd2 = document.getElementById("hands_");
-	        var selectedValue2 = dd2.options[dd2.selectedIndex].value
-	        alert(selectedValue2);
-
-	    }
-
-
-
-	};
+		var selectDocID = document.getElementById("node_resource");
+		var selectedValueinDropdown = selectDocID.options[selectDocID.selectedIndex].value;
 	
-	function doCheck()
-	{
-	 var ddl = document.getElementById("node_resource");
-	 var selectedValue = ddl.options[ddl.selectedIndex].value;
-	    if (selectedValue == "disabled")
-	    {
-	    	alert("Please Select Harvesting Type!");
-	    }
-	};
+	    var arrayLength = myStringArray_node_resource.length;
+		if (selectedValueinDropdown == "disabled")
+		{
+			alert("Please Select Harvesting Type!");
+			exit();
+		}
+		else
+		{
+
+		    for (var i = 0; i < arrayLength; i++) 
+		    {	
+
+		    	if (selectedValueinDropdown == myStringArray_node_resource[i])
+		    	{	
+
+		    		var selectDocID2 = document.getElementById(myStringArray_node_resource[i]);
+			        var selectedValueinSubDropdown = selectDocID2.options[selectDocID2.selectedIndex].value;
+
+			 
+			        var subarr = eval("myStringArray_" + myStringArray_node_resource[i]);
+			        var arrayLength2 = subarr.length;
+
+			        
+
+					for (var ii = 0; ii < arrayLength2; ii++) 
+					{
+						
+					if (selectedValueinSubDropdown == subarr[ii])
+						{
+							alert("subarr[ii] = " + subarr[ii] + "  & myStringArray_node_resource = " + myStringArray_node_resource[i] );
+							ii = arrayLength2;
+							i = arrayLength;
+						}
+					}
+			       
+		        }
+
+		   	}
+
+		}
+	}
 });
